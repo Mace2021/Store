ebef7d8b75ba19e5466c9a69a25fda57c612ce12
Elvis <79801444+Mace2021@users.noreply.github.com>
2023-07-22 15:36:04 -0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,11 @@
+const products = [
+  { image: '/images/Garri.JPG', title: 'Garri', numberOfItems: 10, description: [{ weight: 'lbs', price: '$50' }] },
+  { image: '/images/Oil.JPG', title: 'Oil', numberOfItems: 0, description: [{ weight: '5lbs', price: '$50' }] },
+  { image: '/images/Cat Fish.jpg', title: 'Cat Fish', numberOfItems: 5, description: [{ weight: '5lbs', price: '$50' }] },
+  { image: 'gari.png', title: 'Product 4', numberOfItems: 10, description: [{ weight: '5lbs', price: '$50' }] },
+  { image: 'gari.png', title: 'Product 5', numberOfItems: 2, description: [{ weight: '5lbs', price: '$50' }] },
+  { image: 'gari.png', title: 'Product 6', numberOfItems: 8, description: [{ weight: '5lbs', price: '$50' }] },
+];
+
+export default products;
+
