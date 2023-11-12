6013c9f3fa5c7f1025263dd5479b782f78171646
Elvis <79801444+Mace2021@users.noreply.github.com>
2023-11-12 14:37:03 -0700

    update

--- data.js
+++ data.js
@@ -2,8 +2,8 @@ const products = [
   { image: '/images/Garri.JPG', title: 'Garri', numberOfItems: 10, description: [{ weight: 'lbs', price: '$50' }] },
   { image: '/images/Oil.JPG', title: 'Oil', numberOfItems: 0, description: [{ weight: '5lbs', price: '$50' }] },
   { image: '/images/Cat Fish.jpg', title: 'Cat Fish', numberOfItems: 5, description: [{ weight: '5lbs', price: '$50' }] },
-  { image: 'gari.png', title: 'Product 4', numberOfItems: 10, description: [{ weight: '5lbs', price: '$50' }] },
-  { image: 'gari.png', title: 'Product 5', numberOfItems: 2, description: [{ weight: '5lbs', price: '$50' }] },
+  { image: '/images/cowskinpic.png', title: 'Cow Skin', numberOfItems: 10, description: [{ weight: '5lbs', price: '$50' }] },
+  { image: '/images/Sardinefish.png', title: ' Sardine fish (Shawa)', numberOfItems: 2, description: [{ weight: '5lbs', price: '$50' }] },
   { image: 'gari.png', title: 'Product 6', numberOfItems: 8, description: [{ weight: '5lbs', price: '$50' }] },
 ];
 
