d58fbe479ea177125b30fc3626d834fb3a1bfd7b
Elvis <79801444+Mace2021@users.noreply.github.com>
2023-12-06 22:48:51 -0700

    Cart update

--- script.js
+++ script.js
@@ -1,195 +1,158 @@
 import products from './data.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  let cart = [];
+  let cart = JSON.parse(localStorage.getItem('cart')) || [];
 
   function updateCartIcon() {
     const cartIcon = document.querySelector('.cart-icon');
-    if (cartIcon) {
-      cartIcon.textContent = "🛒" + cart.length;
-    }
+    const itemCount = cart.reduce((count, item) => count + item.quantity, 0);
+    cartIcon.textContent = `🛒 ${itemCount}`;
   }
-
-  function calculateTotal(product) {
-    let total = 0;
-    for (let i = 0; i < cart.length; i++) {
-      total += parseFloat(cart[i].description[0].price);
-    }
-    return total.toFixed(2);
-  }
-
-  function addToCart(product) {
-    cart.push(product);
-    updateCartIcon();
-    localStorage.setItem('cart', JSON.stringify(cart));
-    updateCartPage();
-  }
-
-  function removeFromCart(index) {
-    cart.splice(index, 1);
-    updateCartIcon();
-    localStorage.setItem('cart', JSON.stringify(cart));
-    updateCartPage();
-  }
-
-
-  function calculateSubtotal(product) {
-    return product.numberOfItems * product.description[0].price.toFixed(2);
-  }
-  
-  function calculateTotal() {
-    let total = 0;
-    cart.forEach((product) => {
-      total += calculateSubtotal(product);
-    });
-    return total.toFixed(2);
-  }
-  
-
-
-
-
-  function updateCartPage() {
-    const cartItems = document.getElementById('cartItems');
-    if (cartItems) {
-      cartItems.innerHTML = '';
-      cart.forEach((product, index) => {
-        // Set default value for numberOfItems if not set
-        if (product.numberOfItems === undefined) {
-          product.numberOfItems = 1;
-        }
-        
-        const cartItem = document.createElement('div');
-        
-        // Create elements for displaying product information
-        const productInfo = document.createElement('span');
-        productInfo.textContent = `${product.title} - $${product.description[0].price}`;
-        cartItem.appendChild(productInfo);
-  
-        const itemCount = document.createElement('span');
-        itemCount.textContent = "  - # in cart: ";
-        cartItem.appendChild(itemCount);
-  
-        const itemQuantityInput = document.createElement('input');
-        itemQuantityInput.type = 'number';
-        itemQuantityInput.value = product.numberOfItems;
-        itemQuantityInput.addEventListener('change', (event) => {
-          const newValue = parseInt(event.target.value, 10);
-          if (!isNaN(newValue) && newValue >= 0) {
-            product.numberOfItems = newValue;
-            updateCartPage(); // Update the cart page after changing the quantity
-          }
-        });
-        itemCount.appendChild(itemQuantityInput);
-  
-        const deleteButton = document.createElement('button');
-        deleteButton.textContent = 'Delete';
-        deleteButton.addEventListener('click', () => removeFromCart(index));
-        cartItem.appendChild(deleteButton);
-  
-        cartItems.appendChild(cartItem);
-      });
-  
-      const total = calculateTotal();
-      const totalDiv = document.getElementById('total');
-      if (totalDiv) {
-        totalDiv.textContent = `Total: $${total}`;
-      }
-    }
-  }
-  
-
-  
-
-
-
-
-
-
-
 
   function createProductCard(product) {
     const card = document.createElement('div');
     card.classList.add('product-card');
 
-  const image = document.createElement('img');
-  image.src = product.image;
-  // Add a click event listener to the image
-  image.addEventListener('click', () => {
-    // Toggle a class to make the image bigger
-    image.classList.toggle('enlarged');
-  });
-  card.appendChild(image);
+    const image = document.createElement('img');
+    image.src = product.image;
+    image.addEventListener('click', () => {
+      image.classList.toggle('enlarged');
+    });
+    card.appendChild(image);
 
-  const title = document.createElement('h3');
-  title.textContent = product.title;
-  card.appendChild(title);
+    const title = document.createElement('h3');
+    title.textContent = product.title;
+    card.appendChild(title);
 
-  const stockStatus = document.createElement('p');
-  stockStatus.textContent = product.numberOfItems > 0 ? 'In Stock' : 'Out of Stock';
-  stockStatus.classList.add(product.numberOfItems > 0 ? 'in-stock' : 'out-of-stock');
-  card.appendChild(stockStatus);
+    const stockStatus = document.createElement('p');
+    stockStatus.textContent = product.numberOfItems > 0 ? 'In Stock' : 'Out of Stock';
+    stockStatus.classList.add(product.numberOfItems > 0 ? 'in-stock' : 'out-of-stock');
+    card.appendChild(stockStatus);
 
-  const description = document.createElement('p');
-  const weight = product.description[0].weight;
+    const description = document.createElement('p');
+    const weight = product.description[0].weight;
+    const price = product.description[0].price;
+    description.textContent = `Weight: ${weight}, Price: ${price}`;
+    card.appendChild(description);
 
-  const price = product.description[0].price;
-  description.textContent = `Weight: ${weight}, Price: ${price}`;
-  card.appendChild(description);
-
-   const addToCartButton = document.createElement('button');
+    const addToCartButton = document.createElement('button');
     addToCartButton.textContent = 'Add to Cart';
     addToCartButton.addEventListener('click', () => {
-    if (product.numberOfItems > 0) {
       alert(`Added ${product.title} to cart.`);
-      product.numberOfItems--;
-      stockStatus.textContent = product.numberOfItems > 0 ? 'In Stock' : 'Out of Stock';
-      stockStatus.classList.remove('in-stock', 'out-of-stock');
-      stockStatus.classList.add(product.numberOfItems > 0 ? 'in-stock' : 'out-of-stock');
+      cart.push({
+        id: product.id,
+        title: product.title,
+        price: product.description[0].price,
+        quantity: 1
+      });
+      localStorage.setItem('cart', JSON.stringify(cart));
+      updateCart();
+      updateCartIcon();
+    });
+    card.appendChild(addToCartButton);
 
-      // Add the product to the cart
-      addToCart(product);
-    } else {
-      alert(`${product.title} is out of stock.`);
-    }
-  });
-  card.appendChild(addToCartButton);
+    const decrementButton = document.createElement('button');
+    decrementButton.textContent = '-';
+    decrementButton.addEventListener('click', () => {
+      const existingItem = cart.find(item => item.id === product.id);
+      if (existingItem && existingItem.quantity > 0) {
+        existingItem.quantity--;
+        updateCart();
+        updateCartIcon();
+      }
+    });
+    
 
-  return card;
-}
-
-
-
-const productContainer = document.getElementById('product-container');
-if (productContainer) {
-  products.forEach((product) => {
-    const productCard = createProductCard(product);
-    productContainer.appendChild(productCard);
-  });
-}
-
-
-const checkoutButton = document.createElement('button');
-checkoutButton.textContent = 'Checkout';
-checkoutButton.addEventListener('click', () => {
-  window.location.href = 'cart.html'; // Replace with your payment page URL
-});
-
-function displayCartItems() {
-  if (productContainer) {
-    productContainer.appendChild(checkoutButton);
+    return card;
   }
-}
 
-// Update cart page on load
-const storedCart = localStorage.getItem('cart');
-if (storedCart) {
-  cart = JSON.parse(storedCart);
+  function updateCart() {
+    const cartContainer = document.getElementById('cart');
+    cartContainer.innerHTML = '';
+  
+    let total = 0;
+    cart.forEach(item => {
+      const cartItem = document.createElement('div');
+      cartItem.classList.add('cart-item');
+      
+  
+      const name = document.createElement('span');
+      name.textContent = item.title;
+      
+      cartItem.appendChild(name); 
+      const price = document.createElement('span');
+      price.textContent = item.price.toFixed(2);
+      cartItem.appendChild(price);
+      
+  
+      const quantity = document.createElement('span');
+      quantity.textContent = `Qty: ${item.quantity}`; 
+      cartItem.appendChild(quantity);
+
+      const decrementButton = document.createElement('button');
+      decrementButton.textContent = '-';
+      decrementButton.addEventListener('click', () => {
+        if (item.quantity > 0) {
+          item.quantity--;
+          updateCart();
+          updateCartIcon();
+        }
+      });
+      cartItem.appendChild(decrementButton);
+  
+      
+  
+      const incrementButton = document.createElement('button');
+      incrementButton.textContent = '+';
+      incrementButton.addEventListener('click', () => {
+        item.quantity++;
+        updateCartIcon();
+        updateCart();
+       
+      });
+      cartItem.appendChild(incrementButton);
+  
+      
+  
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = 'Delete';
+      deleteButton.addEventListener('click', () => {
+        const existingItemIndex = cart.findIndex(cartItem => cartItem.id === item.id);
+        if (existingItemIndex !== -1) {
+          cart.splice(existingItemIndex, 1);
+          updateCart();
+          updateCartIcon();
+        }
+      });
+      cartItem.appendChild(deleteButton);
+  
+      cartContainer.appendChild(cartItem);
+
+      total += item.price * item.quantity;
+    });
+  
+    const totalPrice = document.createElement('div');
+    totalPrice.textContent = `Total Price: $${total.toFixed(2)}`;
+    cartContainer.appendChild(totalPrice);
+
+    // Checkout button
+    const checkoutButton = document.createElement('button');
+    checkoutButton.textContent = 'Checkout';
+    cartContainer.appendChild(checkoutButton);
+  }
+  
+
+  const productContainer = document.getElementById('product-container');
+  if (productContainer) {
+    products.forEach((product) => {
+      const productCard = createProductCard(product);
+      productContainer.appendChild(productCard);
+    });
+  }
+
+  updateCart();
   updateCartIcon();
-  updateCartPage();
-}
 });
-
 
 
 // Function to display search results in the product-container
@@ -231,44 +194,4 @@ function searchProducts(searchTerm) {
     product.title.toLowerCase().includes(searchTerm)
   );
 }
-
-
-// Placeholder for createProductCard function - replace with your implementation
-
-// Function to create product card with hyperlink
-function createProductCard(product) {
-  // Create a container div for the product card
-  const card = document.createElement('div');
-  card.classList.add('product-card');
-
-  // Create an anchor element for the hyperlink
-  const productLink = document.createElement('a');
-  productLink.href = '#'; // Replace with the actual URL
-
-  // Create an image element for the product image
-  const image = document.createElement('img');
-  image.src = product.image;
-  image.alt = product.title;
-  productLink.appendChild(image);
-
-  // Create an h3 element for the product title
-  const title = document.createElement('h3');
-  title.textContent = product.title;
-  productLink.appendChild(title);
-
-  // Create a p element for product description
-  const description = document.createElement('p');
-  const weight = product.description[0].weight;
-  const price = product.description[0].price;
-  description.textContent = `Weight: ${weight}, Price: $${price}`;
-  productLink.appendChild(description);
-
-  // Append the anchor element to the product card container
-  card.appendChild(productLink);
-
-  return card;
-}
-
-
-
 
